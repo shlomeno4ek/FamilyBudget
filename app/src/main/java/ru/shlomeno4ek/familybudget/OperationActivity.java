a0bfcdfd6735135bc036af66d76aaa84f313a45b
shlomeno4ek <shlomeno4ek@mail.ru>
2017-02-10 16:49:31 +0300

    pass name purse from ViewPurse in OperationActivity

--- app/src/main/java/ru/shlomeno4ek/familybudget/OperationActivity.java
+++ app/src/main/java/ru/shlomeno4ek/familybudget/OperationActivity.java
@@ -1,13 +1,36 @@
 package ru.shlomeno4ek.familybudget;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class OperationActivity extends AppCompatActivity {
+
+    private String idPurse;
+    private TextView _tvNamePurse;
+    private EditText _etName;
+    private EditText _etDate;
+    private EditText _etSumm;
+    private Spinner _spinner;
+    private Button _btnAccept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operation);
+
+        Intent intent = getIntent();
+        idPurse = intent.getStringExtra("position");
+
+        _tvNamePurse = (TextView) findViewById(R.id.tvNamePurse);
+        _tvNamePurse.setText(intent.getStringExtra("namePurse"));
+
+
     }
 }
