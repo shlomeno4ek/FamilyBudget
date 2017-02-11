2459498a3ba6f065ba014b19e0de4938313de8ea
shlomeno4ek <shlomeno4ek@mail.ru>
2017-02-11 20:47:53 +0300

    add listener for RadioButton

--- app/src/main/java/ru/shlomeno4ek/familybudget/OperationActivity.java
+++ app/src/main/java/ru/shlomeno4ek/familybudget/OperationActivity.java
@@ -3,8 +3,10 @@ package ru.shlomeno4ek.familybudget;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -19,6 +21,9 @@ public class OperationActivity extends AppCompatActivity {
     private EditText _etSumm;
     private Spinner _spinner;
     private Button _btnAccept;
+    private RadioButton _radio_inner;
+    private RadioButton _radio_external;
+    private RadioButton _radio_translation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,33 @@ public class OperationActivity extends AppCompatActivity {
         _tvNamePurse = (TextView) findViewById(R.id.tvNamePurse);
         _tvNamePurse.setText(intent.getStringExtra("namePurse"));
 
+        _spinner = (Spinner) findViewById(R.id.spinner);
+
+        _radio_inner = (RadioButton) findViewById(R.id.radio_inner);
+        _radio_inner.setOnClickListener(radioButtonClickListener);
+
+        _radio_external = (RadioButton) findViewById(R.id.radio_external);
+        _radio_external.setOnClickListener(radioButtonClickListener);
+
+        _radio_translation = (RadioButton) findViewById(R.id.radio_translation);
+        _radio_translation.setOnClickListener(radioButtonClickListener);
 
     }
+
+    View.OnClickListener radioButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RadioButton rb = (RadioButton)v;
+            switch (rb.getId()) {
+                case R.id.radio_inner: _spinner.setEnabled(false);
+                    break;
+                case R.id.radio_external: _spinner.setEnabled(false);
+                    break;
+                case R.id.radio_translation: _spinner.setEnabled(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
