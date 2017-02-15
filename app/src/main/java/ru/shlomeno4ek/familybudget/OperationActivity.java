9d590fee950b4c4f7a01c70299aa3bb128b88c3c
shlomeno4ek <shlomeno4ek@mail.ru>
2017-02-15 16:49:34 +0300

    add processing press buttinAcsept

--- app/src/main/java/ru/shlomeno4ek/familybudget/OperationActivity.java
+++ app/src/main/java/ru/shlomeno4ek/familybudget/OperationActivity.java
@@ -2,6 +2,7 @@ package ru.shlomeno4ek.familybudget;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +12,10 @@ import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -27,6 +30,7 @@ public class OperationActivity extends AppCompatActivity {
     private Spinner _spinnerNamePurse;
     private Spinner _spinnerProcentReserve;
     private Button _btnAccept;
+    private RadioGroup _radio_group;
     private RadioButton _radio_inner;
     private RadioButton _radio_external;
     private RadioButton _radio_translation;
@@ -38,6 +42,8 @@ public class OperationActivity extends AppCompatActivity {
     int myMonth = c.get(Calendar.MONTH);
     int myDay = c.get(Calendar.DAY_OF_MONTH);
 
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +51,8 @@ public class OperationActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         idPurse = intent.getStringExtra("id");
+
+        _etSumm = (EditText) findViewById(R.id.etSumm);
 
         _tvNamePurse = (TextView) findViewById(R.id.tvNamePurse);
         _tvNamePurse.setText(intent.getStringExtra("namePurse"));
@@ -74,6 +82,8 @@ public class OperationActivity extends AppCompatActivity {
         });
 
 
+        _radio_group = (RadioGroup) findViewById(R.id.radio_group);
+
         _radio_inner = (RadioButton) findViewById(R.id.radio_inner);
         _radio_inner.setOnClickListener(radioButtonClickListener);
 
@@ -82,6 +92,31 @@ public class OperationActivity extends AppCompatActivity {
 
         _radio_translation = (RadioButton) findViewById(R.id.radio_translation);
         _radio_translation.setOnClickListener(radioButtonClickListener);
+
+        context = OperationActivity.this;
+
+        _btnAccept = (Button) findViewById(R.id.btnAccept);
+        _btnAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (_etSumm.getText().toString().equals("") || _tvInputDate.getText().toString().equals(R.string.inputDateOperation) || _etName.getText().toString().equals("") || (_radio_external.isChecked() || _radio_inner.isChecked() || _radio_translation.isChecked())) {
+                    Toast.makeText(context, "Не все поля заполнены", Toast.LENGTH_LONG).show();
+                } else {
+                    double summ = Double.parseDouble(_etSumm.getText().toString());
+                    String date = _tvInputDate.getText().toString();
+                    String name = _etName.getText().toString();
+                    int summReserve = 0;
+                    //Получаем id переключателя
+                    int type = _radio_group.getCheckedRadioButtonId();
+                    if (type==0) {
+                        CheckBox _checkBoxPutInReserve = (CheckBox) findViewById(R.id.checkBoxPutInReserve);
+                        if (_checkBoxPutInReserve.isChecked()) {
+                            summReserve += summ/100*Integer.parseInt(_spinnerProcentReserve.getSelectedItem().toString());
+                        }
+                    }
+                }
+            }
+        });
 
     }
 
